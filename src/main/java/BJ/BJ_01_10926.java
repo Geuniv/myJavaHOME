75c2901e00f17a970a2db53f3d793605e753ae78
Geuniv <kkoowl99@gmail.com>
2023-04-01 20:06:24 +0900

    백준문제

---
+++ src/main/java/BJ/BJ_01_10926.java
@@ -0,0 +1,13 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class BJ_01_10926 {
+// 처음에 문제 해석을 잘못해 if - else문을 사용했지만 그냥 출력만 하면 되는 문제였음 ;
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String id = s.next();
+        System.out.println(id + "??!");
+    }
+}
