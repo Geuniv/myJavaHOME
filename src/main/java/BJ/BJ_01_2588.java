8f58bcc56a70a357c8375e9889d06d6f9de0ff45
Geuniv <kkoowl99@gmail.com>
2023-04-07 07:31:28 +0900

    2023.04.06 잔디심기 ( 백준문제 )

--- src/main/java/BJ/BJ_01_2588.java
+++ src/main/java/BJ/BJ_01_2588.java
@@ -13,8 +13,11 @@ public class BJ_01_2588 {
         i = s.nextInt();
         j = s.nextInt();
 
+        // j의 값에서 1의자리 숫자인 5만사용할것이므로 나머지를 5로 나오게 구해서 출력
         System.out.printf("%d \n", i * ( j%10 ) );
+        // 8로 곱해야하는데 나머지로는 나오지 않아서 10으로 먼저 나눈후 몫 38에서 10으로 나눠 나머지 8로 계산
         System.out.printf("%d \n", i * ( j / 10 % 10 ) );
+        // 3은 385를 100으로 나누어 몫 3을 구해 사용하면 되므로 100으로 나눠줌.
         System.out.printf("%d \n", i * ( j / 100 ) );
         System.out.printf("%d", i * j );
     }
