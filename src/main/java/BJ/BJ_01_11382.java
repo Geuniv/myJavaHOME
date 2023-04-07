ce15c18b4ca828033b23430974518f3df3379e6e
Geuniv <kkoowl99@gmail.com>
2023-04-07 22:33:47 +0900

    2023.04.07 잔디심기 ( 백준문제 )

---
+++ src/main/java/BJ/BJ_01_11382.java
@@ -0,0 +1,17 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class BJ_01_11382 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        long a = s.nextLong();
+        long b = s.nextLong();
+        long c = s.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
