6880357187da0726618c74fd7a3b1fa698f4be32
Geuniv <kkoowl99@gmail.com>
2023-04-15 23:36:32 +0900

    2023.04.15 잔디심기 ( if문 예제 )

---
+++ src/main/java/Study/Web_if_01.java
@@ -0,0 +1,17 @@
+package Study;
+
+public class Web_if_01 {
+// if문 예제
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("실행 1"); // 실행
+        }
+        if(false){ // false
+            System.out.println("실행 2");
+            System.out.println("실행 3");
+        }
+        if(3!=3) // false
+            System.out.println("실행 4");
+        System.out.println("실행 5"); // 실행
+    }
+}
