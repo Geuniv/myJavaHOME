09064120bd0d6dedc599bd50aacc9fa8c2ece0fe
Geuniv <kkoowl99@gmail.com>
2023-04-29 00:50:35 +0900

    2023.04.28 잔디심기 ( 별찍기 예제 )

---
+++ src/main/java/Study/Web_Star_07.java
@@ -0,0 +1,16 @@
+package Study;
+
+public class Web_Star_07 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
