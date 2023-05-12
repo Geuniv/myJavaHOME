2872ffa15b8fbcb4f35effb015fa934f28182387
Geuniv <kkoowl99@gmail.com>
2023-05-12 14:44:06 +0900

    2023.05.12 잔디 심기 ( 교재 객체지향 예제 )

---
+++ src/main/java/Ex11/Ex11_03.java
@@ -0,0 +1,47 @@
+package Ex11;
+
+class Car3 {
+
+    /* 필드에 private 접근 제어 수식어를 붙인다. */
+    private String color;
+    private int speed;
+
+    public void upSpeed(int value) {
+        this.speed = this.speed + value;
+    }
+
+    public void downSpeed(int value) {
+        this.speed = this.speed - value;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    /* color 필드의 값을 변경시켜 주는 메소드이다. */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /* speed 필드의 값을 변경시켜 주는 메소드이다. */
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
+public class Ex11_03 {
+
+    public static void main(String[] args) {
+
+        Car3 myCar1 = new Car3();
+        myCar1.setColor("빨강"); /* myCar1.color는 오류 */
+        myCar1.setSpeed(0); /* myCar1.speed는 오류 */
+        myCar1.upSpeed(30);
+
+        /* myCar1 자동차의 색상과 속도를 출력한다. */
+        System.out.printf("자동차1의 색상은 " + myCar1.getColor() + " 이며 현재속도는 " + myCar1.getSpeed() + "km 입니다.");
+    }
+}
