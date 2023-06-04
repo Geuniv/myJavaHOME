6c7eba186da175bbe94dc4c75c9a77ff144f5c5f
Geuniv <kkoowl99@gmail.com>
2023-06-04 23:39:42 +0900

    2023.06.04 잔디심기 ( 교재 예제 )

---
+++ src/main/java/Ex06/Ex_06_07.java
@@ -0,0 +1,17 @@
+package Ex06;
+
+public class Ex_06_07 {
+
+    public static void main(String[] args) {
+
+        int hap; /* 합계를 누적할 변수를 선언한다. */
+        int i; /* 1부터 10까지 변하는 변수를 선언한다. */
+
+        /* for 문에 의해 1부터 10까지 10회 반복된다. */
+        for (i = 1; i <= 10; i++) {
+            hap = hap + i; /* hap 변수에 1부터 10까지 반복해서 누적한다. */
+        }
+
+        System.out.printf("1에서 10까지의 합 : %d \n", hap);
+    }
+}
