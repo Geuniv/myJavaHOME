cc2b514b789960d81dc36e4547a95611376f6b5e
Geuniv <kkoowl99@gmail.com>
2023-07-02 23:27:52 +0900

    2023.07.02 잔디심기 ( 시험 예제 )

---
+++ src/main/java/Study/Si_Q_01.java
@@ -0,0 +1,15 @@
+package Study;
+
+public class Si_Q_01 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("*");
+            for (int j = 0; j < i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
