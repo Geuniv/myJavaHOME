29ee594ecf33f9015b81ed18a40cfea6893d07d0
Geuniv <kkoowl99@gmail.com>
2024-01-06 23:53:01 +0900

    2023.01.06 잔디심기 ( 백준 예제 )

--- src/main/java/BJ/BJ_03_8393.java
+++ src/main/java/BJ/BJ_03_8393.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class BJ_03_8393 {
     
     // Scanner 를 사용한 방식
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -18,4 +17,29 @@ public class BJ_03_8393 {
         }
         System.out.println(sum);
     }
+
+    // BufferReader를 이용한 방식
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//        br.close();
+//        int s = 0;
+//
+//        for( int i = 1 ; i <= N ; i++ ) s+=i;
+//
+//        System.out.println(s);
+//    }
+
+    // 간략화 시킨 코드
+//    public static void main(String[] args) throws IOException {
+//
+//        int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+//        int s = 0;
+//
+//        for( int i = 1 ; i <= N ; i++ ) s+=i;
+//
+//        System.out.println(s);
+//    }
 }
