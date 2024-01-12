58813b53676de0a1936c3783bf44351f73cac695
Geuniv <kkoowl99@gmail.com>
2024-01-12 16:59:32 +0900

    2023.01.12 잔디심기 ( 백준 예제 )

--- src/main/java/BJ/BJ_03_15552.java
+++ src/main/java/BJ/BJ_03_15552.java
@@ -1,24 +1,49 @@
 package BJ;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class BJ_03_15552 {
+    
+    // BufferReader 사용
     public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    // StringBuffer 사용
+    /*public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine()," ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            System.out.println(a+b);
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
         }
-    }
+        br.close();
+
+        System.out.println(sb);
+
+    }*/
 }
