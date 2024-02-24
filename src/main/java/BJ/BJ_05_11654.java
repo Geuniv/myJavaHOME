957dd2721d5797c35e390e322c0d6bcfa19e6355
Geuniv <kkoowl99@gmail.com>
2024-02-24 23:22:11 +0900

    2023.02.24 잔디심기 ( 백준 예제 )

---
+++ src/main/java/BJ/BJ_05_11654.java
@@ -0,0 +1,22 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class BJ_05_11654 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int ch = in.next().charAt(0);
+
+        System.out.print(ch);
+    }
+
+//    public static void main(String[] args) throws Exception {
+//
+//        int a = System.in.read();
+//        System.out.print(a);
+//
+//    }
+}
