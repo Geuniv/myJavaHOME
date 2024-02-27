86e9f4ab0ad03cc2944b8c72473560f00c95f61b
Geuniv <kkoowl99@gmail.com>
2024-02-27 23:45:59 +0900

    2024.02.27 잔디심기 ( 백준 예제 )

--- src/main/java/BJ/BJ_05_11720.java
+++ src/main/java/BJ/BJ_05_11720.java
@@ -1,8 +1,5 @@
-package org.example;
+package BJ;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BJ_05_11720 {
@@ -22,18 +19,18 @@ public class BJ_05_11720 {
         System.out.print(sum);
     }
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();	// N 은 쓸모가 없으므로 입력만 받는다.
-
-        int sum = 0;
-
-        for(byte value : br.readLine().getBytes()) {
-            sum += (value - '0');	// 또는 (a-48)
-        }
-
-        System.out.print(sum);
-
-    }
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        br.readLine();	// N 은 쓸모가 없으므로 입력만 받는다.
+//
+//        int sum = 0;
+//
+//        for(byte value : br.readLine().getBytes()) {
+//            sum += (value - '0');	// 또는 (a-48)
+//        }
+//
+//        System.out.print(sum);
+//
+//    }
 }
